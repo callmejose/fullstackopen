5400109b71ccb030f693afb737d278ddba76a141
callmejose <ojosedo@gmail.com>
2022-05-12 11:08:23 -0500

    2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,22 +1,4 @@
-const Course = ({ courses }) => {
-  const exercisesReducer = (sum, part) => sum + part.exercises
-
-  return (
-    <><h1>Web development course</h1>
-      <ul>{courses.map((course) => (
-        <li key={course.name}><div>
-          <h2>{course.name}</h2>
-          <ul>{course.parts.map((part) => {
-            return (
-              <li key={part.name}>{part.name} {part.exercises}</li>
-            )
-          })}</ul>
-          <p><b>total of {course.parts.reduce(exercisesReducer, 0)} exercises</b></p>
-        </div></li>
-      ))}</ul>
-    </>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [{
